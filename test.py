863b2a4c337a4871cfe643c8f54da49818484829
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2020-02-02 22:18:43 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,92 @@
+import logging
+import sys
+import time
+import utils
+import json,hashlib
+
+MINING_DIFFICULTY=3
+MINING_SENDER = "THE BLOCKCHAIN"
+MINING_REWARD = 1.0
+
+logging.basicConfig(level=logging.INFO,stream=sys.stdout)
+logger = logging.getLogger(__name__)
+
+class BlockChain(object):
+
+    def __init__(self,blockchain_adress=None):
+       self.transaction_pool = []
+       self.chain = []
+       self.create_block(0,self.hash({}))
+       self.blockchain_adress = blockchain_adress 
+
+    def create_block(self, nonce,previous_hash):
+        block = utils.sorted_dict_by_key({
+            "timestamp" : time.time(),
+            "transactions" : self.transaction_pool,
+            "nonce" : nonce,
+            "previous_hash" : previous_hash
+        })
+        self.chain.append(block)
+        self.transaction_pool = []
+        return block
+
+    def hash(self,block):
+        sorted_block = json.dumps(block,sort_keys=True)
+        return hashlib.sha256(sorted_block.encode()).hexdigest()
+
+    def add_transaction(self,sender_blockechain_adress,recipient_blockchain_adress,value):
+        transaction =utils.sorted_dict_by_key({
+          "sender_blockechain_adress":sender_blockechain_adress,
+          "recipient_blockchain_adress":recipient_blockchain_adress,
+          "value":float(value)  
+        })
+        self.transaction_pool.append(transaction)
+        return True
+
+    def valid_proof(self,transactions,previous_hash,nonce,difficulty=MINING_DIFFICULTY):
+        guess_block = utils.sorted_dict_by_key({
+            "transactions":transactions,
+            "nonce":nonce,
+            "previous_hash":previous_hash
+        })
+        guess_hash=self.hash(guess_block)
+        return guess_hash[:difficulty] == "0"*difficulty
+
+    def proof_work(self):
+        transactions = self.transaction_pool.copy()
+        previous_hash = self.hash(self.chain[-1])
+        nonce = 0
+        while self.valid_proof(transactions,previous_hash,nonce) is False:
+            nonce += 1
+        return nonce
+
+    def mining(self):
+        self.add_transaction(
+            sender_blockechain_adress = MINING_SENDER,
+            recipient_blockchain_adress = self.blockchain_adress,
+            value = MINING_REWARD
+        )
+        nonce = self.proof_work()
+        previous_hash = self.hash(self.chain[-1])
+        self.create_block(nonce,previous_hash)
+        logger.info({"action":"mining","status":"success"})
+        return True
+
+
+
+if __name__ == "__main__":
+    my_blockchain_address ="my_blockchain_address"
+    block_chain = BlockChain(blockchain_adress=my_blockchain_address)
+    utils.pprint(block_chain.chain)
+
+    block_chain.add_transaction("A","B",1.0)
+    block_chain.mining()
+    utils.pprint(block_chain.chain)
+
+    block_chain.add_transaction("C","D",2.0)
+    block_chain.add_transaction("X","Y",3.0)
+    block_chain.mining()
+    utils.pprint(block_chain.chain)
+
+
+
